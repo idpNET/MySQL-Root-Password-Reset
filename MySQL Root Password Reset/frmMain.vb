8bb20e24eccc82db55cf6a581c744248cfad8354
Keyvan Hasani (idpNET admin) <k1.dsec993@gmail.com>
2023-07-18 13:14:42 +0330

    Update code comments Code optimizations

--- MySQL Root Password Reset/frmMain.vb
+++ MySQL Root Password Reset/frmMain.vb
@@ -7,9 +7,11 @@ Public Class frmMain
     Private stopwatch As Stopwatch ' Declare the Stopwatch object
     Private once_announced As Boolean = False ' Flag to track operation result detection and announcement
     Private isCancellationRequested As Boolean ' Flag to track cancellation request
+
+    ' mysqld.exe browsing logic
     Private Sub btnBrowse1_Click(sender As Object, e As EventArgs) Handles btnBrowse1.Click
         Dim openFileDialog1 As New OpenFileDialog()
-
+        ' Set filter to only accept mysqld.exe
         openFileDialog1.Filter = "mysqld.exe|mysqld.exe"
         openFileDialog1.Title = "Select mysqld.exe"
 
@@ -29,9 +31,10 @@ Public Class frmMain
         End If
     End Sub
 
+    ' my.ini browsing logic
     Private Sub btnBrowse2_Click(sender As Object, e As EventArgs) Handles btnBrowse2.Click
         Dim openFileDialog2 As New OpenFileDialog()
-
+        ' Set filter to only accept my.ini
         openFileDialog2.Filter = "my.ini|my.ini"
         openFileDialog2.Title = "Select my.ini"
 
@@ -51,23 +54,20 @@ Public Class frmMain
         End If
     End Sub
 
+    ' Start password change operation
     Private Sub btnSET_Click(sender As Object, e As EventArgs) Handles btnSET.Click
-        DisableControlls()
-        rtbNotification.Text = ""
-        stopwatch = Stopwatch.StartNew()
+        DisableControls()
+        rtbNotification.Text = "" ' Reset notification before new operations 
+        stopwatch = Stopwatch.StartNew() ' Start tracking process time
         isCancellationRequested = False
         bwRun.RunWorkerAsync()
-
     End Sub
 
+    ' MySQL service auto detection logic
     Private Sub btnDetect_Click(sender As Object, e As EventArgs) Handles btnDetect.Click
-
-
-
         ' Attempt to auto-detect the MySQL service name
         Dim serviceName As String = FindMySQLServiceName()
         If Not String.IsNullOrEmpty(serviceName) Then
-
             txtServiceName.Text = serviceName
             lblStatus4.Text = "OK"
             lblStatus4.ForeColor = Color.FromArgb(0, 64, 0)
@@ -80,6 +80,7 @@ Public Class frmMain
         End If
     End Sub
 
+    ' MySQL service auto detection logic
     Private Function FindMySQLServiceName() As String
         ' Find a service name starting with "mysql"
         Dim services As ServiceController() = ServiceController.GetServices()
@@ -92,6 +93,7 @@ Public Class frmMain
         Return Nothing
     End Function
 
+    ' Stop MySQL service
     Private Sub StopMySQLService()
         Dim serviceName As String = txtServiceName.Text
         Try
@@ -102,10 +104,10 @@ Public Class frmMain
             End If
         Catch ex As Exception
             Throw New Exception("Failed to stop MySQL service: " & ex.Message & Environment.NewLine)
-
         End Try
     End Sub
 
+    ' Start MySQL service
     Private Sub StartMySQLService()
         Dim serviceName As String = txtServiceName.Text
         Try
@@ -116,10 +118,10 @@ Public Class frmMain
             End If
         Catch ex As Exception
             Throw New Exception("Failed to start MySQL service: " & ex.Message & Environment.NewLine)
-
         End Try
     End Sub
 
+    ' Terminate instances of mysqld.exe to release resource for further operations
     Private Sub KillMySQLDProcesses()
         Try
             Dim processes As Process() = Process.GetProcessesByName("mysqld")
@@ -129,16 +131,11 @@ Public Class frmMain
             Next
         Catch ex As Exception
             Throw New Exception("Failed to kill mysqld.exe process: " & ex.Message & Environment.NewLine)
-
         End Try
     End Sub
 
-
-
-
-
-
-
+    ' Write text to notification 
+    ' Write text to notification 
     Private Sub AppendTextToNotification(ByVal text As String)
         If rtbNotification.InvokeRequired Then
             rtbNotification.Invoke(Sub() AppendTextToNotification(text))
@@ -146,8 +143,6 @@ Public Class frmMain
             Dim startIndex As Integer = rtbNotification.TextLength
             rtbNotification.AppendText(text & Environment.NewLine)
             rtbNotification.ScrollToCaret() ' Scroll to the end of the text
-        End If
-    End Sub
 
             ' Check for specific strings and apply formatting
             Dim searchString1 As String = "Operation completed in"
@@ -169,15 +164,8 @@ Public Class frmMain
         End If
     End Sub
 
-    Private Sub frmMain_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
-
-    End Sub
-
-    Private Sub gbInputParameters_Enter(sender As Object, e As EventArgs) Handles gbInputParameters.Enter
-
-    End Sub
-
+    ' Password check logic
     Private Sub txtPassword_TextChanged(sender As Object, e As EventArgs) Handles txtPassword.TextChanged
         If (txtPassword.TextLength >= 5) Then
             lblStatus3.Text = "OK"
@@ -192,12 +180,14 @@ Public Class frmMain
         End If
     End Sub
 
+    ' Service name manual input logic
     Private Sub txtServiceName_TextChanged(sender As Object, e As EventArgs) Handles txtServiceName.TextChanged
-
         lblStatus4.Text = "Manually set"
         lblStatus4.ForeColor = Color.SaddleBrown
         lblStatus4.BackColor = Color.Khaki
     End Sub
+
+    ' Run mysqld.exe command to change password
     Sub RunCommandAsAdministrator(ByVal command As String, ByVal workingDirectory As String)
         Try
             Dim processInfo As New ProcessStartInfo("cmd.exe")
@@ -224,11 +214,11 @@ Public Class frmMain
 
             process.WaitForExit()
         Catch ex As Exception
-            AppendTextToNotification("Failed to run command: " & ex.Message)
-            bwRun.CancelAsync()
+            Throw New Exception("Failed to run command: " & ex.Message)
         End Try
     End Sub
 
+    ' Write mysqld.exe outputs to notifications
     Private Sub Process_OutputDataReceived(sender As Object, e As DataReceivedEventArgs)
         If e.Data IsNot Nothing Then
             ' Append the output to the notification control
@@ -236,6 +226,7 @@ Public Class frmMain
         End If
     End Sub
 
+    ' Write mysqld.exe output errors to notifications
     Private Sub Process_ErrorDataReceived(sender As Object, e As DataReceivedEventArgs)
         If e.Data IsNot Nothing Then
             ' Append the error output to the notification control
@@ -243,38 +234,36 @@ Public Class frmMain
         End If
     End Sub
 
+    ' Operation status check logic
     Private Sub rtbNotification_TextChanged(sender As Object, e As EventArgs) Handles rtbNotification.TextChanged
         Dim text As String = rtbNotification.Text
 
         ' Check for success or failure strings
         If text.IndexOf("ready for connections. Version", StringComparison.OrdinalIgnoreCase) >= 0 Then
-            If (Not once_accounced) Then
+            If (Not once_announced) Then
                 ' Operation is successful, release all cmd instances
                 AppendTextToNotification("Password successfully changed. Killing mysqld.exe processes ...")
                 KillMySQLDProcesses()
-                once_accounced = True
+                once_announced = True
             End If
-
         ElseIf text.IndexOf("Shutdown", StringComparison.OrdinalIgnoreCase) >= 0 Or text.IndexOf("Program aborted", StringComparison.OrdinalIgnoreCase) >= 0 Then
-            If (Not once_accounced) Then
+            If (Not once_announced) Then
                 ' Operation is unsuccessful
                 AppendTextToNotification("Password change failed. Killing mysqld.exe processes ...")
                 AppendTextToNotification("")
                 KillMySQLDProcesses()
-                once_accounced = True
+                once_announced = True
             End If
-
         End If
-
-
-
     End Sub
 
+    ' BackgroundWorker reportage
     Private Sub bwRun_ProgressChanged(sender As Object, e As ProgressChangedEventArgs) Handles bwRun.ProgressChanged
         ProgressBar.Value = e.ProgressPercentage
         AppendTextToNotification(e.UserState.ToString())
     End Sub
 
+    ' BackgroundWorker completion
     Private Sub bwRun_RunWorkerCompleted(sender As Object, e As RunWorkerCompletedEventArgs) Handles bwRun.RunWorkerCompleted
         stopwatch.Stop()
 
@@ -288,9 +277,13 @@ Public Class frmMain
         stopwatch.Reset()
 
         ' Enable the button
-        EnableControlls()
+        EnableControls()
+        ' Reset announcement flag
+        once_announced = False
     End Sub
-    Sub DisableControlls()
+
+    ' Disable UI controls to prevent conflicts in process procedure
+    Sub DisableControls()
         txtPassword.Enabled = False
         txtPathToMyIni.Enabled = False
         txtPathToMysqld.Enabled = False
@@ -301,7 +294,8 @@ Public Class frmMain
         btnSET.Enabled = False
     End Sub
 
-    Sub EnableControlls()
+    ' ReEnable UI controls after process completion
+    Sub EnableControls()
         txtPassword.Enabled = True
         txtPathToMyIni.Enabled = True
         txtPathToMysqld.Enabled = True
@@ -312,7 +306,7 @@ Public Class frmMain
         btnSET.Enabled = True
     End Sub
 
-    End Sub
+    ' ' BackgroundWorker Work
     Private Sub bwRun_DoWork(sender As Object, e As System.ComponentModel.DoWorkEventArgs) Handles bwRun.DoWork
         ' Check if cancellation is requested before starting any operations
         If isCancellationRequested Then
@@ -321,25 +315,26 @@ Public Class frmMain
         End If
 
         Try
-
-
+            ' Variable decelerations for password change operation
             Dim mysqldPath As String = txtPathToMysqld.Text
             Dim myIniPath As String = txtPathToMyIni.Text
             Dim newPassword As String = txtPassword.Text
+            Dim mysqlBinDirectory As String = Path.GetDirectoryName(mysqldPath) ' Dir to mysqld.exe file
+            Dim tempFolderPath As String = Path.GetTempPath() ' Get windows temp folder
+            Dim tempFileName As String = Path.GetRandomFileName() ' Get a random file name
+            Dim tempFilePath As String = Path.Combine(tempFolderPath, tempFileName) ' Dir to temp file to be used in mysqld command
+            Dim mysqlCommand As String = $"mysqld --defaults-file=""{myIniPath}"" --init-file=""{tempFilePath}"" --console""" ' mysqld command to change password
 
-            Dim tempFolderPath As String = Path.GetTempPath()
-            Dim tempFileName As String = Path.GetRandomFileName()
-            Dim tempFilePath As String = Path.Combine(tempFolderPath, tempFileName)
-            Dim mysqlBinDirectory As String = Path.GetDirectoryName(mysqldPath)
-            Dim mysqlCommand As String = $"mysqld --defaults-file=""{myIniPath}"" --init-file=""{tempFilePath}"" --console"""
             ' Write the alter statement to the temp file
             File.WriteAllText(tempFilePath, $"alter user 'root'@'localhost' identified by '{newPassword}';")
+
             ' Check if cancellation is requested before starting any operations
             If isCancellationRequested Then
                 e.Cancel = True ' Set the cancellation flag in the event args
                 Return ' Exit the method
             End If
-            ' Perform the MySQL operations here
+
+
 
             ' Report progress to update the progress bar
             bwRun.ReportProgress(0, "Stopping MySQL service... (UI might look unresponsive for a while)")
@@ -350,6 +345,7 @@ Public Class frmMain
                 e.Cancel = True ' Set the cancellation flag in the event args
                 Return ' Exit the method
             End If
+
             bwRun.ReportProgress(10, "mysqld.exe processes killed.")
             KillMySQLDProcesses()
 
@@ -358,6 +354,7 @@ Public Class frmMain
                 e.Cancel = True ' Set the cancellation flag in the event args
                 Return ' Exit the method
             End If
+
             bwRun.ReportProgress(30, "Attempting to change root user password ...")
             RunCommandAsAdministrator(mysqlCommand, mysqlBinDirectory)
 
@@ -367,8 +364,7 @@ Public Class frmMain
                 Return ' Exit the method
             End If
 
-
-            bwRun.ReportProgress(70, "Restarting MySQL service ... (UI might look unresponsive for a while)")
+            bwRun.ReportProgress(80, "Restarting MySQL service ... (UI might look unresponsive for a while)")
             StartMySQLService()
 
             ' Check if cancellation is requested before starting any operations
@@ -376,6 +372,7 @@ Public Class frmMain
                 e.Cancel = True ' Set the cancellation flag in the event args
                 Return ' Exit the method
             End If
+            File.Delete(tempFilePath)
             bwRun.ReportProgress(100, "")
         Catch ex As Exception
             ' Handle the exception and request cancellation
@@ -383,11 +380,7 @@ Public Class frmMain
             e.Cancel = True ' Set the cancellation flag in the event args
         End Try
     End Sub
-
-    Private Sub lblTip1_Click(sender As Object, e As EventArgs) Handles lblTip1.Click
-
-    End Sub
-
+    ' Set tool tip
     Private Sub lblTip1_MouseHover(sender As Object, e As EventArgs) Handles lblTip1.MouseHover
         ToolTip1.SetToolTip(lblTip1, "By default, it should be in : " &
                             Environment.NewLine &
@@ -395,19 +388,19 @@ Public Class frmMain
 
     End Sub
 
-
+    ' Set tool tip
     Private Sub lblTip2_MouseHover(sender As Object, e As EventArgs) Handles lblTip2.MouseHover
         ToolTip1.SetToolTip(lblTip2, "By default, it should be in : " &
                           Environment.NewLine &
                           "<windows installation drive>\ProgramData\MySQL\<MySQL Server <Version>\my.ini")
     End Sub
-
+    ' Set tool tip
     Private Sub lblTip1_MouseClick(sender As Object, e As MouseEventArgs) Handles lblTip1.MouseClick
         ToolTip1.SetToolTip(lblTip1, "By default, it should be in : " &
                           Environment.NewLine &
                           "<windows installation drive>\Program Files\MySQL\<MySQL Server <Version>\mysql.exe")
     End Sub
-
+    ' Set tool tip
     Private Sub lblTip2_MouseClick(sender As Object, e As MouseEventArgs) Handles lblTip2.MouseClick
         ToolTip1.SetToolTip(lblTip2, "By default, it should be in : " &
                         Environment.NewLine &
